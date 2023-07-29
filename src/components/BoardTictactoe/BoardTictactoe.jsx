39e19794a77450a642b285c9135ff339573395f7
Brisna A. Paez M <124035708+Brisnayu@users.noreply.github.com>
2023-07-29 11:42:32 +0200

    funtions tic tac toe

--- src/components/BoardTictactoe/BoardTictactoe.jsx
+++ src/components/BoardTictactoe/BoardTictactoe.jsx
@@ -7,7 +7,7 @@ import ModalInformation from "../ModalInformation/ModalInformation";
 import { RulesPlayTicTacToe } from "../../functions/RulesGames";
 import ScoreTable from "./ScoreTable/ScoreTable";
 import ModalResult from "./ModalResult/ModalResult";
-import { changedBoardTicTacToe } from "../../utils/FunctionsTicTacToe";
+import { changedBoardTicTacToe, getArraySolutions } from "../../utils/FunctionsTicTacToe";
 
 const BoardTictactoe = () => {
   const originalBoardTicTacToe = [
@@ -38,77 +38,6 @@ const BoardTictactoe = () => {
   const [winner, setWinner] = useState(false);
 
   const [boardTicTacToe, setBoardTicTacToe] = useState(originalBoardTicTacToe);
-
-  const getArraySolutions = () => {
-    const getWinner = () => {
-      setWinner(true);
-      setOpen1(true);
-      if (turn === "X") {
-        setWinnerX(winnerX + 1);
-      } else if (turn === "O") {
-        setWinnerO(winnerO + 1);
-      }
-    };
-
-    if (
-      boardTicTacToe[0][0] === boardTicTacToe[1][0] &&
-      boardTicTacToe[1][0] === boardTicTacToe[2][0] &&
-      boardTicTacToe[0][0] !== null
-    ) {
-      getWinner();
-      return setWinnerPosition("vertical");
-    } else if (
-      boardTicTacToe[0][1] === boardTicTacToe[1][1] &&
-      boardTicTacToe[1][1] === boardTicTacToe[2][1] &&
-      boardTicTacToe[0][1] !== null
-    ) {
-      getWinner();
-      return setWinnerPosition("vertical");
-    } else if (
-      boardTicTacToe[0][2] === boardTicTacToe[1][2] &&
-      boardTicTacToe[1][2] === boardTicTacToe[2][2] &&
-      boardTicTacToe[0][2] !== null
-    ) {
-      getWinner();
-      return setWinnerPosition("vertical");
-    } else if (
-      boardTicTacToe[0][0] === boardTicTacToe[1][1] &&
-      boardTicTacToe[1][1] === boardTicTacToe[2][2] &&
-      boardTicTacToe[0][0] !== null
-    ) {
-      getWinner();
-      return setWinnerPosition("diagonal");
-    } else if (
-      boardTicTacToe[0][2] === boardTicTacToe[1][1] &&
-      boardTicTacToe[1][1] === boardTicTacToe[2][0] &&
-      boardTicTacToe[0][2] !== null
-    ) {
-      getWinner();
-      return setWinnerPosition("diagonal");
-    }
-
-    for (let i = 0; i < 3; i++) {
-      const horizontalComparative = boardTicTacToe[i].reduce(
-        (acc, next) =>
-          acc && next === boardTicTacToe[i][0] && boardTicTacToe[i][0] !== null,
-        true,
-      );
-
-      if (horizontalComparative === true) {
-        getWinner();
-        return setWinnerPosition("horizontal");
-      }
-    }
-
-    const result = boardTicTacToe.flat();
-    const prueba = result.find((valor) => valor === null);
-
-    if (prueba !== null) {
-      // console.log("no hay ganador y no hay más casillas!")
-      setOpen2(true);
-      setTied(tied + 1);
-    }
-  };
 
   const resetGame = () => {
     setBoardTicTacToe(originalBoardTicTacToe);
@@ -199,7 +128,20 @@ const BoardTictactoe = () => {
                         indexRow,
                         indexColumn,
                       );
-                      getArraySolutions();
+                      getArraySolutions(
+                        setWinner,
+                        setOpen1,
+                        setWinnerX,
+                        winnerX,
+                        setWinnerO,
+                        winnerO,
+                        boardTicTacToe,
+                        setWinnerPosition,
+                        setOpen2,
+                        setTied,
+                        turn,
+                        tied
+                      );
                     }}
                   >
                     {column}
--- src/utils/FunctionsTicTacToe.js
+++ src/utils/FunctionsTicTacToe.js
@@ -18,3 +18,86 @@ export const changedBoardTicTacToe = (
   setBoardTicTacToe(position);
 };
 
+export const getArraySolutions = (
+  setWinner,
+  setOpen1,
+  setWinnerX,
+  winnerX,
+  setWinnerO,
+  winnerO,
+  boardTicTacToe,
+  setWinnerPosition,
+  setOpen2, 
+  setTied,
+  turn, 
+  tied
+) => {
+  const getWinner = () => {
+    setWinner(true);
+    setOpen1(true);
+    if (turn === "X") {
+      setWinnerX(winnerX + 1);
+    } else if (turn === "O") {
+      setWinnerO(winnerO + 1);
+    }
+  };
+
+  if (
+    boardTicTacToe[0][0] === boardTicTacToe[1][0] &&
+    boardTicTacToe[1][0] === boardTicTacToe[2][0] &&
+    boardTicTacToe[0][0] !== null
+  ) {
+    getWinner();
+    return setWinnerPosition("vertical");
+  } else if (
+    boardTicTacToe[0][1] === boardTicTacToe[1][1] &&
+    boardTicTacToe[1][1] === boardTicTacToe[2][1] &&
+    boardTicTacToe[0][1] !== null
+  ) {
+    getWinner();
+    return setWinnerPosition("vertical");
+  } else if (
+    boardTicTacToe[0][2] === boardTicTacToe[1][2] &&
+    boardTicTacToe[1][2] === boardTicTacToe[2][2] &&
+    boardTicTacToe[0][2] !== null
+  ) {
+    getWinner();
+    return setWinnerPosition("vertical");
+  } else if (
+    boardTicTacToe[0][0] === boardTicTacToe[1][1] &&
+    boardTicTacToe[1][1] === boardTicTacToe[2][2] &&
+    boardTicTacToe[0][0] !== null
+  ) {
+    getWinner();
+    return setWinnerPosition("diagonal");
+  } else if (
+    boardTicTacToe[0][2] === boardTicTacToe[1][1] &&
+    boardTicTacToe[1][1] === boardTicTacToe[2][0] &&
+    boardTicTacToe[0][2] !== null
+  ) {
+    getWinner();
+    return setWinnerPosition("diagonal");
+  }
+
+  for (let i = 0; i < 3; i++) {
+    const horizontalComparative = boardTicTacToe[i].reduce(
+      (acc, next) =>
+        acc && next === boardTicTacToe[i][0] && boardTicTacToe[i][0] !== null,
+      true,
+    );
+
+    if (horizontalComparative === true) {
+      getWinner();
+      return setWinnerPosition("horizontal");
+    }
+  }
+
+  const result = boardTicTacToe.flat();
+  const prueba = result.find((valor) => valor === null);
+
+  if (prueba !== null) {
+    // console.log("no hay ganador y no hay más casillas!")
+    setOpen2(true);
+    setTied(tied + 1);
+  }
+};
